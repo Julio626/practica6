2fd625299d4980d335f2f1c27bdb3d9aeb3f6065
Julio626 <20235918@esen.edu.sv>
2025-03-23 22:50:26 -0600

    .

---
+++ src/routes.ts
@@ -0,0 +1,31 @@
+import { Router, Request, Response } from "express";
+import User from "./models/Users";
+import { createAccount, login } from "./controllers/UserController";
+import { body } from "express-validator";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Bienvenido a la API")
+});
+
+router.post("/auth/register", [
+    body("name").isString().notEmpty().withMessage("Name is required"),
+    body("password").isString().isLength({min: 8}).withMessage("Password must be at least 8 characters"),
+    body("email").isEmail().withMessage("Invalid email"),
+    body("username").isString().notEmpty().withMessage("Username is required")],
+    async (req, res) => {
+        createAccount(req, res);
+    }
+);
+
+router.post("/auth/login", [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password").notEmpty().withMessage("Password must be at least 8 characters long"),
+    ],
+    async (req, res) => {
+        login(req, res);
+    }
+)
+
+export default router;
